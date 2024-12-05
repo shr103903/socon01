aa8d9803bcf3a2917fd4a1fc4e4ed9505d69eafe
shr103903 <shr103903@gmail.com>
2024-12-05 13:22:13 +0900

    test_upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
-function Food({name, picture}) {
-    return (
-    <div>
-    <h1>I like {name}</h1>
-    <img src={picture} />
-    </div>
-    );
+class App extends React.Component {
+    state = {
+        count: 0,
+    };
+
+    add = () => {
+        this.setState(current => ({
+            count: current.count + 1,
+        }));
+    };
+
+    minus = () => {
+        this.setState(current => ({
+            count: current.count - 1,
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
+    }
 }
 
-const foodILike = [
-{
-    name: 'Kimchi',
-    image: 'https://cdn.kormedi.com/wp-content/uploads/2024/12/gettyimages-a11200652.jpg.webp',
-},
-{
-    name: 'Samgyeopsal',
-    image: 'https://cdn.mindgil.com/news/photo/202105/71358_8221_3033.jpg',
-}  
-];
-
-function App() {
-    return(
-    <div>
-        {foodILike.map(dish => (
-            <Food name={dish.name} picture={dish.image}/>))}
-    </div>
-    );
-}
 export default App;

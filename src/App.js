3e7eae745c7e7509045ce6c04f7e82b59a9ed908
shr103903 <shr103903@gmail.com>
2024-12-05 12:41:57 +0900

    test_upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({name, picture}) {
+    return (
+    <div>
+    <h1>I like {name}</h1>
+    <img src={picture} />
+    </div>
+    );
+}
+
+const foodILike = [
+{
+    name: 'Kimchi',
+    image: 'https://cdn.kormedi.com/wp-content/uploads/2024/12/gettyimages-a11200652.jpg.webp',
+},
+{
+    name: 'Samgyeopsal',
+    image: 'https://cdn.mindgil.com/news/photo/202105/71358_8221_3033.jpg',
+}  
+];
 
 function App() {
     return(
     <div>
-        <h1>hello</h1>
-        <Potato />
+        {foodILike.map(dish => (
+            <Food name={dish.name} picture={dish.image}/>))}
     </div>
     );
 }
